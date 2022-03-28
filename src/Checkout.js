5a3d2282f478aae9199b70ac4283dfa0219ab630
majdkilou <majdonak@hotmail.com>
2022-03-28 02:24:47 +0200

    cheack

--- src/Checkout.js
+++ src/Checkout.js
@@ -1,24 +1,25 @@
-import React from "react";
-import "./Checkout.css";
-import Subtotal from "./Subtotal";
-import { useStateValue } from "./StateProvider";
-import CheckoutProduct from "./CheckoutProduct";
-
+import React from 'react';
+import './Checkout.css';
+import Subtotal from './Subtotal';
+import { useStateValue } from './StateProvider';
+import CheckoutProduct from './CheckoutProduct';
 
 function Checkout() {
-    const [{ basket, user }, dispatch] = useStateValue();
+  const [{ basket, user }, dispatch] = useStateValue();
 
-    return (
-        <div className="checkout">
-            <div className="checkout_left">
-                <img className='checkout_ad' 
-                src ="https://images-na.ssl-images-amazon.com/images/G/02/UK_CCMP/TM/OCC_Amazon1._CB423492668_.jp"
-                 alt=""/>
-                <div>
+  return (
+    <div className="checkout">
+      <div className="checkout_left">
+        <img
+          className="checkout_ad"
+          src="https://images-na.ssl-images-amazon.com/images/G/02/UK_CCMP/TM/OCC_Amazon1._CB423492668_.jp"
+          alt=""
+        />
+        <div>
           <h3>Hello, {user?.email}</h3>
-          <h2 className="checkout__title">Your shopping Basket</h2>
+          <h2 className="checkout__title">Your shopping Basket?</h2>
 
-          {basket.map(item => (
+          {basket.map((item) => (
             <CheckoutProduct
               id={item.id}
               title={item.title}
@@ -27,16 +28,13 @@ function Checkout() {
               rating={item.rating}
             />
           ))}
-
         </div>
-            </div>
-            <div className="checkout_right">
-                <Subtotal/>
-                
-
-            </div>
-        </div>
-    )
+      </div>
+      <div className="checkout_right">
+        <Subtotal />
+      </div>
+    </div>
+  );
 }
 
-export default Checkout
+export default Checkout;
